f348c1c9de6915df850ab03af667eec03e789711
PoisTome <sr.zu3ira@gmail.com>
2023-11-17 18:14:48 -0300

    teste2

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+
+float salario;
+
+printf("Digite seu salario: ");
+scanf("f",&salario);
+
+if (salario < 1320){
+    printf("\nSeu salario esta abaixo do minimo");
+} else if (salario = 1320){
+    printf("\nSeu salario e minimo");
+}else 
+    printf("\nSeu salario esta acima do minimo");
+
+printf("\nO salario minimo atual equivale a R$ 1320");
+
+
+
+
+
+}
+
